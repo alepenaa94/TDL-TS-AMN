e22c9f97b4d4a488766a76b7836659935db5b695
Alejandro Pena <alee.pena.94@gmail.com>
2022-06-11 01:18:38 -0300

    Agregamos la primer version del Ahorcado

---
+++ client/src/components/ahorcado/PalabraOfuscada.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+
+
+class PalabraOfuscada extends React.Component {
+    private word_len: number = 0;
+
+    constructor(props:any){
+        super(props);
+    }
+
+    public setLetter(letter:string , idx:number ) {        
+        let letter_span = document.getElementById('letter_'+idx);
+        if (letter_span) {
+            letter_span.innerHTML = letter;
+        } else {
+            console.log("algo paso y no existe le elmeento a setear!");
+        }
+    }
+
+    componentDidMount() {
+        //Ejemplo base del fetch
+        // fetch("https://gorest.co.in/public/v2/posts/")
+        // .then(response => response.json())
+        // .then(data => {
+        //     this.lista_juegos = data;
+        // });
+        
+        // deberiamos ir al backend con un fetch para obtener la palabra ..
+        this.word_len = 5;
+        // actualizamos lo que se tiene que mostrar para la palabra..
+        this.forceUpdate();
+    }  
+
+    InitOfuscado() {
+        let i:number = 0;
+        let _arr:Array<any> = [];
+        while (i < this.word_len){
+            _arr.push(<span id={'letter_' + i} className='letter'></span>);
+            i++;
+        }
+        return _arr;
+    }
+
+    render(): React.ReactNode {
+        return (
+            <div className="word" id="word">
+                {this.InitOfuscado()}
+            </div>
+        );
+    }
+}
+
+export default PalabraOfuscada; 
--- client/src/pages/Ahorcado.tsx
+++ client/src/pages/Ahorcado.tsx
@@ -1,15 +1,90 @@
 import React from 'react';
 
-export default function Ahorcado() {
+import Figura from '../components/ahorcado/Figura.tsx';
+import PalabraOfuscada from '../components/ahorcado/PalabraOfuscada.tsx';
+import CInput from '../components/Input.tsx';
+
+
+
+class Ahorcado extends React.Component {
+
+    private pal_ofsc:any = null;
+    private input_area:any = null;
+    private figura:any = null;
+
+    constructor(props:any){
+        super(props);
+        this.pal_ofsc = React.createRef();
+        this.input_area = React.createRef();
+        this.figura = React.createRef();
+    }
+
+
+
+    funcion_error() {
+        if (this.figura.current.show_more()==false) {
+            alert("SE TERMINO EL JUEGO!");
+        }
+    }
+
     
-    return (
+    private idx_temp:number = 0;
 
-        <div className="container">
-            <div className="row justify-content-center">
-                <section id="ahorcado" className='amn-page text-center'> ESTE ES EL JUEGO DEL AHORCADO. </section>
-            </div>
-        </div>   
+    clickFn() {
 
-    )
+        let str:string = this.input_area.current.getValue();
+        //console.log(str);
+        let len:number = str.length;
+        if (len==0) {
+            alert("ESTA VACIA inserta algo..");
+        } else if (len == 1) {
+            // ACA deberiamos ir al back y validar si la letra es correcta..
+            // Seteamos la letra!
+            this.pal_ofsc.current.setLetter(str,this.idx_temp);
 
+            if (this.idx_temp == 4){
+                this.idx_temp = 0;
+
+                this.funcion_error();
+
+            } else {
+                this.idx_temp++;
+            }
+        } else {
+            alert("Solamente se puede insertar un caracter..");
+        }
+    }
+
+
+    
+    render(): React.ReactNode {
+        return (
+
+            <div className="container">
+                <div className="row justify-content-center">
+                    <section id="ahorcado" className='amn-page text-center h1'> ESTE ES EL JUEGO DEL AHORCADO. 
+                        <div className='game-container'>
+                            <Figura ref={this.figura} />
+                            
+                            <PalabraOfuscada ref={this.pal_ofsc} />
+                            
+                            <div>
+                                <CInput ref={this.input_area} />
+                                <button onClick={e=>this.clickFn()}
+                                        className='btn-group button mt-6'>
+                                        Validar letra
+                                </button>
+                            </div>
+                            
+                        </div>
+                        
+    
+                    </section>
+                </div>
+            </div>   
+    
+        )   
+    }
 }
+
+export default Ahorcado; 
