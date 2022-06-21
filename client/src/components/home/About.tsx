086e61bf0bbeb2de22a50fffe6639236e139a47f
M. Iñaki Otegui <mati.otegui@gmail.com>
2022-06-21 20:51:58 -0300

    more info about us

--- client/src/components/home/About.tsx
+++ client/src/components/home/About.tsx
@@ -8,8 +8,8 @@ export default function About() {
                 <h2 className="page-section-heading text-center text-uppercase text-secondary mb-0">Acerca de </h2>
 
                 <div className="row">
-                    <div className="col-lg-4 ms-auto"><p className="lead">Somos un grupo de la materia Teoría del Lenguaje de Programación de la FIUBA.</p></div>
-                    <div className="col-lg-4 me-auto"><p className="lead">Esta web fue hecha con React, Typescript y Bootstrap con el objetivo de diseñar una página para juegos de niños.</p></div>
+                    <div className="col-lg-4 ms-auto"><p className="lead">Somos el grupo <strong>AMN</strong> de la materia Teoría del Lenguaje de Programación de la FIUBA. Los integrantes son: <strong>Nerea Piccone, Alejandro Peña, Matías Iñaki Otegui.</strong></p></div>
+                    <div className="col-lg-4 me-auto"><p className="lead">Esta web fue hecha con <strong>React</strong>, <strong>Typescript</strong> y <strong>Bootstrap</strong> con el objetivo de diseñar una página para juegos de niños.</p></div>
                 </div>
 
             </div>
